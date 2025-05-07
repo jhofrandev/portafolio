9f35db8ee2196fd3f9e1a6688151a41c69bed055
Jhofran Briceño <jhofran.dev@outlook.com>
2025-05-07 15:35:09 -0500

    refactor: update Menu and Option components for improved functionality and organization

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ import Option from "./Option";
 export default function Menu() {
   return (
     <article className="pt-4 mx-4">
-      <Option typeOption={0} nameOption="Información">
+      <Option typeOption={1} nameOption="Información">
         <ul className="pl-4 text-neutral-500 text-sm font-[VT323]">
           <li className="flex gap-1">
             <i className="fa-regular fa-file-lines"></i>
@@ -20,20 +20,38 @@ export default function Menu() {
         </ul>
       </Option>
 
-      <Option typeOption={0} nameOption="Proyectos">
-        <ul className="pl-4 text-neutral-500 text-sm font-[VT323]">
-          <li className="flex gap-1">
-            <i className="fa-regular fa-file-lines"></i>
-            sobre_mi.pdf
-          </li>
-          <li className="flex gap-1">
-            <i className="fa-regular fa-file-lines"></i>
-            experiencia.pdf
-          </li>
-          <li className="flex gap-1">
-            <i className="fa-regular fa-file-lines"></i>
-            habilidades.pdf
-          </li>
+      <Option typeOption={1} nameOption="Proyectos">
+        <ul className="h-32 border border-dotted border-[#B9B28A] rounded-lg pl-4 py-1 grid grid-cols-1 overflow-auto text-neutral-500 text-sm font-[VT323]">
+          <div>
+            <li className="flex gap-1 items-center">
+              <i className="fa-regular fa-file-lines"></i>
+              sobre_mi.pdf
+              <span className="rounded bg-[#2DAA9E] px-0.5 text-xs text-white font-medium">
+                LAB
+              </span>
+            </li>
+            <li className="flex gap-1 items-center">
+              <i className="fa-regular fa-file-lines"></i>
+              experiencia.pdf
+              <span className="rounded bg-[#F3C623] px-0.5 text-xs text-white font-medium">
+                PERS
+              </span>
+            </li>
+            <li className="flex gap-1 items-center">
+              <i className="fa-regular fa-file-lines"></i>
+              habilidades.pdf
+              <span className="rounded bg-[#B8001F] px-0.5 text-xs text-white font-medium">
+                PRIV
+              </span>
+            </li>
+            <li className="flex gap-1 items-center">
+              <i className="fa-regular fa-file-lines"></i>
+              sobre_mi.pdf
+              <span className="rounded bg-[#2DAA9E] px-0.5 text-xs text-white font-medium">
+                LAB
+              </span>
+            </li>
+          </div>
         </ul>
       </Option>
 
--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -11,26 +11,28 @@ export default function Option({
   typeOption,
   nameOption,
 }: OptionProps) {
-  const [isOpenOption, setIsOpenOption] = useState(true);
+  const [isOpenOption, setIsOpenOption] = useState(false);
 
   const handleClick = () => {
     setIsOpenOption(!isOpenOption);
   };
 
   return (
-    <button className="mb-2 block" onClick={handleClick}>
-      <span className="flex gap-1 items-center text-neutral-500 font-[VT323]">
-        {typeOption === 0 && isOpenOption ? (
-          <i className="fa-solid fa-folder-open"></i>
-        ) : (
-          <i className="fa-solid fa-folder-closed"></i>
-        )}
-        <span>{nameOption}</span>
-        {typeOption === 0 && isOpenOption && (
-          <i className="text-xs fa-regular fa-square-caret-down"></i>
-        )}
-      </span>
+    <>
+      <button className="block cursor-pointer" onClick={handleClick}>
+        <span className="flex gap-1 items-center text-neutral-500 font-[VT323]">
+          {typeOption === 0 && isOpenOption ? (
+            <i className="fa-solid fa-folder-open"></i>
+          ) : (
+            <i className="fa-solid fa-folder-closed"></i>
+          )}
+          <span>{nameOption}</span>
+          {typeOption === 0 && isOpenOption && (
+            <i className="text-xs fa-regular fa-square-caret-down"></i>
+          )}
+        </span>
+      </button>
       {isOpenOption && children}
-    </button>
+    </>
   );
 }
